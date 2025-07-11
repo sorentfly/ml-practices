6e7d06bcb2c37c117a0cbf15281f4c9adb65d02c
Vlad Smith <sorentad@gmail.com>
2025-07-11 22:07:47 +0300

    Remove unused

--- scripts/backend.sh
+++ scripts/backend.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && cd .. && pwd )"
-DOCKERFILE="Dockerfile"
 ARCH=$(uname -m)
 
 if [[ "$ARCH" == "arm64" ]]; then
-#   DOCKERFILE="Dockerfile-arm64v8"
     echo "Not implemented yet"
     exit
 fi
 
 case "$1" in
   --install)
-    docker build -f "$DIR/docker/$DOCKERFILE" -t smith/python3.12/ai "$DIR"
+    docker build -f "$DIR/docker/Dockerfile" -t smith/python3.12/ai "$DIR"
     docker compose -f "$DIR/docker/docker-compose.yml" up -d
-    # docker compose exec smith-ai python /opt/app/main.py
     ;;
   --reinstall)
     docker compose down -v
@@ -25,5 +22,4 @@ case "$1" in
     ;;
 esac
 
-# shellcheck disable=SC2164
 cd "$DIR"
